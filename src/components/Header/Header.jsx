95bd0b5c6901cf33cadc24444962feaf611099d0
Sanhitasaxena <sanhitasaxena96903@gmail.com>
2023-06-11 18:04:20 +0530

    in progress

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,69 @@
-import React from 'react'
-import { NavContainer, NavItem, NavLink, NavMenu } from './Header.style'
+import React, {useState, useEffect} from "react";
+import {
+  NavContainer,
+  NavItem,
+  NavLink,
+  NavMenu,
+  ToggleButtonDiv,
+  ToggleOpen,
+} from "./Header.style";
+import { Sling as Hamburger} from 'hamburger-react'
 
 
 const Header = () => {
+
+  const [isToggleOpen, setIsToggleOpen] = useState(false)
+   
+  useEffect(()=>{
+      console.log(isToggleOpen? {isToggleOpen} : {isToggleOpen})
+  }, [isToggleOpen])
+
+
   return (
     <>
-    <NavContainer>
-      <NavMenu>
-        <NavItem>
-          <NavLink to="/">HOME</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink>ABOUT</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink to="/content">CONTENT</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink to="experience">EXPERIENCE</NavLink>
-        </NavItem>
-      </NavMenu>
-    </NavContainer>
-    </>
-  )
-}
+      <NavContainer isToggleOpen= {isToggleOpen}>
+        <NavMenu>
+          <NavItem>
+            <NavLink to="/">HOME</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink>ABOUT</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/content">CONTENT</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="experience">EXPERIENCE</NavLink>
+          </NavItem>
+        </NavMenu>
+        <ToggleButtonDiv onClick={()=>{
+          setIsToggleOpen(curr => !curr)
+        }}>
+           <Hamburger size={28} duration={0.8}
+          />
+        </ToggleButtonDiv>
 
-export default Header
+      
+      
+      </NavContainer>
+      {isToggleOpen && (
+               <ToggleOpen>
+               <NavItem>
+                   <NavLink to="/">HOME</NavLink>
+                 </NavItem>
+                 <NavItem>
+                   <NavLink>ABOUT</NavLink>
+                 </NavItem>
+                 <NavItem>
+                   <NavLink to="/content">CONTENT</NavLink>
+                 </NavItem>
+                 <NavItem>
+                   <NavLink to="experience">EXPERIENCE</NavLink>
+                 </NavItem>
+               </ToggleOpen>
+        )}
+    </>
+  );
+};
+
+export default Header;
--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 import { Link } from 'react-router-dom'
 
+
 export const NavContainer = styled.div`
   background-color: pink;
-  height: 60px;
+  height: ${(props) => props.isToggleOpen ? "auto": "70px"};
+  display: flex;
 `;
 
 export const NavMenu = styled.ul`
@@ -13,6 +15,13 @@ export const NavMenu = styled.ul`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+  width: 80%;
+  overflow-x: hidden;
+
+  @media screen and (max-width: 430px){
+    visibility: hidden;
+  }
+
 `;
 export const NavItem = styled.li`
   color: black;
@@ -22,3 +31,23 @@ export const NavLink = styled(Link)`
   text-decoration: none;
   text-align: center;
 `;
+
+export const ToggleButtonDiv = styled.div`
+    display: flex;
+    align-items: center;
+    padding-right: 10px;
+
+`
+
+export const ToggleOpen = styled.ul`
+  list-style: none;
+  line-height: 10px;
+  color: black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media screen and (min-width: 430px){
+    visibility: hidden;
+  }
+`
